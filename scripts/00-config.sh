0fe7b944f39989c3d152cb7f943317980b173ed8
Lucy <lucy@absolucy.moe>
2022-04-21 17:04:35 +0000

    ♻️ Begin work on germinate script

--- scripts/00-config.sh
+++ scripts/00-config.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
+set -e
+
+UBUNTU_VERSION=22.04
+UBUNTU_CODE=jammy
+
+DISTRO_PKGS=(ubuntu-minimal ubuntu-standard pop-desktop)
+LIVE_PKGS=(casper distinst expect gparted pop-installer pop-installer-casper)
+RM_PKGS=(bus-mozc imagemagick-6.q16 irqbalance mozc-utils-gui pop-installer-session snapd ubuntu-session ubuntu-wallpapers unattended-upgrades xul-ext-ubufox yaru-theme-gnome-shell)
+MAIN_POOL=(at dfu-programmer efibootmgr ethtool kernelstub libfl2 lm-sensors pm-utils postfix powermgmt-base python3-debian python3-distro python3-evdev python3-systemd system76-wallpapers xbacklight)
 
 SCRIPTS_DIR="$(dirname "$(readlink -f "$0")")"
 BUILD_DIR="${SCRIPTS_DIR}/../build"
 CACHE_DIR="${BUILD_DIR}/cache"
+
+GERMINATE_DIR="${BUILD_DIR}/germinate"
+GERMINATE_SEEDS_DIR="${GERMINATE_DIR}/seeds/ubuntu.${UBUNTU_CODE}"
+
 ROOTFS_BASE_DIR="${BUILD_DIR}/rootfs.base"
 ROOTFS_LIVE_DIR="${BUILD_DIR}/rootfs.live"
+
 ROOTFS_IMG="${BUILD_DIR}/rootfs.img"
 EFI_IMG="${BUILD_DIR}/efi.img"
 ROOTFS_SQUASHED="${BUILD_DIR}/rootfs.squashfs"
