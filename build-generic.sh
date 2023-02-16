b8af44e21a5f30a4f28ccfb4de674170183e907b
Tobias Heider <me@tobhe.de>
2023-02-16 20:06:25 +0100

    Add optional live build

--- build-generic.sh
+++ build-generic.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+set -e
+
 if [[ "${EUID:-$(id -u)}" != 0 ]]; then
 	echo "This script must be run as root."
 	exit 1
 fi
-
-set -xe
 
 mkdir -p build
 cd build
@@ -13,12 +13,15 @@ cd build
 ../scripts/01-build-base-rootfs.sh
 ../scripts/02-setup-rootfs.sh
 
-../scripts/live/03-setup-efi-img.sh
-../scripts/live/04-setup-live-rootfs.sh
-../scripts/live/05-setup-pool.sh
-../scripts/live/06-build-live-image.sh
+if [[ "$1" == "--live" ]]; then
+	../scripts/live/03-setup-efi-img.sh
+	../scripts/live/04-setup-live-rootfs.sh
+	../scripts/live/05-setup-pool.sh
+	../scripts/live/06-build-live-image.sh
+else
+	../scripts/disk/03-setup-disk-rootfs.sh
+	../scripts/disk/04-build-disk-image.sh
+fi
 
 echo "Done"
  
-# ../scripts/disk/03-setup-disk-rootfs.sh
-# ../scripts/disk/04-build-disk-image.sh
