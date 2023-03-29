fa401a540a3103a05927414b215aafe2c0328482
Tobias Heider <me@tobhe.de>
2023-03-29 23:24:11 +0200

    Update for 23.04 Lunar Lobster

--- scripts/00-config.sh
+++ scripts/00-config.sh
@@ -8,12 +8,12 @@ export LC=C.UTF-8
 # Git doesn't like it when you run git commands in the dir of another user, but this should be safe as it's just a read-only command.
 SOURCE_DATE_EPOCH="$(git --git-dir="$PWD/../.git" log -1 --format='%ct' 2> /dev/null || echo "42")"
 DISTRO_NAME=ubuntu
-DISTRO_VERSION=22.10
+DISTRO_VERSION=23.04
 DISTRO_VOLUME_LABEL="Ubuntu ${DISTRO_VERSION} arm64"
 DISTRO_EPOCH="${SOURCE_DATE_EPOCH}"
 DISTRO_DATE="$(date --date=@"${SOURCE_DATE_EPOCH}" +%Y%m%d)"
-UBUNTU_CODE=kinetic
-UBUNTU_NAME="Kinetic Kudu"
+UBUNTU_CODE=lunar
+UBUNTU_NAME="Lunar Lobster"
 GNOME_INITIAL_SETUP_STAMP=21.04
 
 DISTRO_PKGS=(eatmydata ubuntu-minimal ubuntu-standard ubuntu-asahi linux-firmware)
