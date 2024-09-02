03c63fd603ed3dabbeaefee00e79a10099f572a1
Tobias Heider <me@tobhe.de>
2024-09-02 21:33:42 +0200

    Fix tar numeric owner for usb build.

--- scripts/livefs-to-usb.sh
+++ scripts/livefs-to-usb.sh
@@ -104,7 +104,8 @@ elif find "${ARTIFACT_DIR}"/livecd.ubuntu-asahi.squashfs -quit; then
 elif find "${ARTIFACT_DIR}"/livecd.*.rootfs.tar.gz -quit; then
 	# Format == plain
 	log "Copying to disk"
-	tar -xzf "${ARTIFACT_DIR}"/livecd.*.rootfs.tar.gz -C "${MNT_DIR}"
+	tar -xz --numeric-owner --same-owner -p --xattrs --xattrs-include="*" -f \
+	    "${ARTIFACT_DIR}"/livecd.*.rootfs.tar.gz -C "${MNT_DIR}"
 	mkdir -p "${MNT_DIR}/boot/efi"
 	cp "${ARTIFACT_DIR}"/livecd.*.manifest-remove "${MNT_DIR}"
 fi
