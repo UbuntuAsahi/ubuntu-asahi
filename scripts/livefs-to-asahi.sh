6238c155d5e10377b680d03b41f789394920dc48
Tobias Heider <me@tobhe.de>
2026-05-28 15:13:06 +0200

    Adjust u-boot path to match debian

--- scripts/livefs-to-asahi.sh
+++ scripts/livefs-to-asahi.sh
@@ -149,7 +149,7 @@ rm -f "${MNT_DIR}"/livecd.*.manifest-remove
 
 # Copy bootloaders
 m1n1="${MNT_DIR}/usr/lib/m1n1/m1n1.bin"
-uboot="${MNT_DIR}/usr/lib/u-boot-asahi/u-boot-nodtb.bin"
+uboot="${MNT_DIR}/usr/lib/u-boot/apple_m1/u-boot-nodtb.bin"
 dtbs="${MNT_DIR}/lib/firmware/*/device-tree/apple/*.dtb"
 
 mkdir -p "${MNT_DIR}"/boot/efi/m1n1
